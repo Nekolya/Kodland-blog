ba0fd543d4a18b2a3f436323bafaf6526b541ed3
Olya <olya75825@gmail.com>
2020-08-10 18:50:33 +0300

    Create navbar
    add blog app, include cloudinary and materialize

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Post
 
 # Create your views here.
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+    ordering = ['creation_date']
